d82005e081b3dc0a0040b3507e67f966147151bc
Kevin Kim <kkim@Kevins-MacBook-Air.local>
2021-09-13 16:20:42 -0600

    GET request, setting in state, provider dropdowns

--- src/components/HardwareTargets.js
+++ src/components/HardwareTargets.js
@@ -22,14 +22,121 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "space-between",
   },
 }));
+
+/*
+  [
+  {
+    provider: 'AWS',
+    instances: [
+      {
+        instance: m4.large,
+        cpu: 2,
+        memory: 8
+      },
+      {
+        instance: m4.xlarge,
+        cpu: 4,
+        memory: 16,
+      },
+      {
+        instance: m4.2xlarge,
+        cpu: 8,
+        memory: 32
+      },
+      {
+        instance: m4.4xlarge,
+        cpu: 16,
+        memory: 64
+      }
+    ]
+  },
+  {
+    provider: 'GCP',
+    instances: [
+      {
+        instance: n2-standard-2,
+        cpu: 2,
+        memory: 8
+      },
+      {
+        instance: n2-standard-4
+        cpu: 4,
+        memory: 16,
+      },
+      {
+        instance: n2-standard-8
+        cpu: 8,
+        memory: 32
+      },
+      {
+        instance: n2-standard-16,
+        cpu: 16,
+        memory: 64
+      }
+    ]
+  },
+  }
+]
+*/
+
 const HardwareTargets = () => {
   const classes = useStyles();
+  const [hardwareTargets, setHardwareTargets] = React.useState(null);
+  const [isLoading, setLoading] = React.useState(true);
   const [provider, setProvider] = React.useState("");
   const [instance, setInstance] = React.useState("");
+
+  async function fetchHardwareTargets() {
+    const url = "http://netheria.takehome.octoml.ai/hardware";
+    try {
+      let res = await fetch(url, {
+        method: "GET",
+        // mode: "no-cors",
+      });
+      return await res.json();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function assembleHardwareTargetsData() {
+    let data = await fetchHardwareTargets();
+
+    let final = {};
+
+    const addProviderInstance = (target) => {
+      final[target.provider].instances[target.instance] = {
+        name: target.instance,
+        cpu: target.cpu,
+        memory: target.memory,
+      };
+    };
+
+    data.forEach((target) => {
+      if (final[target.provider]) {
+        addProviderInstance(target);
+      } else {
+        final[target.provider] = {
+          instances: {},
+        };
+        addProviderInstance(target);
+      }
+    });
+    setHardwareTargets(final);
+    setLoading(false);
+  }
+
+  React.useEffect(() => {
+    assembleHardwareTargetsData();
+  }, []);
 
   const handleSetProvider = (event) => setProvider(event.target.value);
 
   const handleSetInstance = (event) => setInstance(event.target.value);
+
+  if (isLoading) {
+    return <h1>LOADING</h1>;
+  }
 
   return (
     <>
@@ -48,12 +155,11 @@ const HardwareTargets = () => {
               value={provider}
               onChange={handleSetProvider}
             >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
+              {Object.keys(hardwareTargets).map((provider) => (
+                <MenuItem key={provider} value={provider}>
+                  {provider}
+                </MenuItem>
+              ))}
             </Select>
           </FormControl>
           <FormControl variant="outlined">
